978fcff850537d930c09f9f6cc5a2517f48b7fe9
aminoxlhd <aminoxlhd@gmail.com>
2024-11-08 21:47:14 +0100

    add Pages

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to PyDeliveryApp</h1>
+      <p>Explore our restaurants and enjoy your meals!</p>
+    </div>
+  );
+};
+
+export default HomePage;
